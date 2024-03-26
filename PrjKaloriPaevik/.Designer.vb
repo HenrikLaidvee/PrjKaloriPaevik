c93795164597d1934fe17cd9e7f0ebf088f79203
HenrikLaidvee <laidvee.henrik@gmail.com>
2024-03-27 01:34:25 +0200

    Add project files.

---
+++ PrjKaloriPaevik/.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.gbDatabase = New System.Windows.Forms.GroupBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.gbInfo = New System.Windows.Forms.GroupBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.gbInfo.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label1.Location = New System.Drawing.Point(22, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(135, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Sisestage toit:"
+        '
+        'gbDatabase
+        '
+        Me.gbDatabase.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.gbDatabase.Location = New System.Drawing.Point(12, 80)
+        Me.gbDatabase.Name = "gbDatabase"
+        Me.gbDatabase.Size = New System.Drawing.Size(512, 338)
+        Me.gbDatabase.TabIndex = 1
+        Me.gbDatabase.TabStop = False
+        Me.gbDatabase.Text = "Valige sobiv toode"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.btnSearch.Location = New System.Drawing.Point(534, 31)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(123, 43)
+        Me.btnSearch.TabIndex = 2
+        Me.btnSearch.Text = "Otsi"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.btnSave.Location = New System.Drawing.Point(663, 31)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(122, 43)
+        Me.btnSave.TabIndex = 3
+        Me.btnSave.Text = "Salvesta"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.btnClear.Location = New System.Drawing.Point(534, 83)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(251, 43)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Puhasta"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.btnExit.Location = New System.Drawing.Point(663, 375)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(122, 43)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Välju"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.btnBack.Location = New System.Drawing.Point(534, 375)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(123, 43)
+        Me.btnBack.TabIndex = 6
+        Me.btnBack.Text = "Tagasi"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'gbInfo
+        '
+        Me.gbInfo.Controls.Add(Me.RichTextBox1)
+        Me.gbInfo.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.gbInfo.Location = New System.Drawing.Point(534, 132)
+        Me.gbInfo.Name = "gbInfo"
+        Me.gbInfo.Size = New System.Drawing.Size(251, 237)
+        Me.gbInfo.TabIndex = 7
+        Me.gbInfo.TabStop = False
+        Me.gbInfo.Text = "Info"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(6, 29)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(239, 202)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.TextBox1.Location = New System.Drawing.Point(163, 31)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(361, 30)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.gbInfo)
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.gbDatabase)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Database aken"
+        Me.gbInfo.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents gbDatabase As GroupBox
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnBack As Button
+    Friend WithEvents gbInfo As GroupBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents TextBox1 As TextBox
+End Class
