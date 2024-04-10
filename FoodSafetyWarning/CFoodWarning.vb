4772b3e77eeacd0ab9c29772ac211493c741069c
janml <janml@LAPTOP-JACRP4N3>
2024-04-10 17:18:04 +0300

    Kerge warning süsteem liigsete kalorite korral

---
+++ FoodSafetyWarning/CFoodWarning.vb
@@ -0,0 +1,22 @@
+﻿Public Class CFoodWarning
+    Implements IFoodWarning
+
+    Private ReadOnly _calorieThreshold As Integer
+
+    Public Sub New(calorieThreshold As Integer)
+        _calorieThreshold = calorieThreshold
+    End Sub
+
+    Public Function IsFoodUnhealthy(foodName As String, calorieContent As Integer) As Boolean Implements IFoodWarning.IsFoodUnhealthy
+        If calorieContent > _calorieThreshold Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+    Public Sub DisplayWarning(foodName As String, calorieContent As Integer) Implements IFoodWarning.DisplayWarning
+        If IsFoodUnhealthy(foodName, calorieContent) Then
+            Console.WriteLine($"Warning: {foodName} is high in calories ({calorieContent} calories per serving) and may not be suitable for consumption.")
+        End If
+    End Sub
+End Class
---
+++ FoodSafetyWarning/IFoodWarning.vb
@@ -0,0 +1,7 @@
+﻿Public Interface IFoodWarning
+
+    Sub DisplayWarning(foodName As String, calorieContent As Integer)
+    Function IsFoodUnhealthy(foodName As String, calorieContent As Integer) As Boolean
+
+
+End Interface
