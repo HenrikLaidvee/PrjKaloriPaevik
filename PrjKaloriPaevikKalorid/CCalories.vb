32dfb80e1050ef3fce62c01e0f987de0fecf5143
Miivanttu <70687706+Miivanttu@users.noreply.github.com>
2024-04-17 11:43:36 +0300

    Writes data to database

--- PrjKaloriPaevikKalorid/CCalories.vb
+++ PrjKaloriPaevikKalorid/CCalories.vb
@@ -6,54 +6,46 @@ Public Class CCalories
 
     Private calorieLimit As Integer
     Private consumedCalories As Integer
-
-    Public Sub New()
-        ' Load saved data when the FitnessTracker instance is created
-
-    End Sub
+    Private connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Throthar\source\repos\PrjKaloriPaevik\FoodDatabase.accdb;"
 
     Public Sub SetCalorieLimit(ByRef limit As Integer) Implements ICalories.SetCalorieLimit
         calorieLimit = limit
-        SaveData() ' Save data after each modification
     End Sub
 
-    Public Sub AddCalories(ByRef calories As Integer) Implements ICalories.AddCalories
+    Public Sub AddCalories(ByRef foodID As Integer) Implements ICalories.AddCalories
+        Dim calories As Integer = getCalories(foodID)
         consumedCalories += calories
-        SaveData() ' Save data after each modification
+        WriteData()
     End Sub
+    Private Function getCalories(ByRef ID As Integer)
+        Dim result As Integer
+        Try
+            Using connection As New OleDbConnection(connectionString)
+                connection.Open()
 
-    Public Function GetRemainingCalories() As Integer Implements ICalories.GetRemainingCalories
-        Return calorieLimit - consumedCalories
+                Dim commandText As String = "SELECT Toit FROM Kalorid WHERE YourPrimaryKeyColumn = ID"
+                Dim command As New OleDbCommand(commandText, connection)
+                ' Specify the value of the primary key to retrieve
+                command.Parameters.AddWithValue("ID", ID)
+                result = command.ExecuteScalar()
+                connection.Close()
+            End Using
+        Catch ex As Exception
+            'MessageBox.Show("Error: " & ex.Message)
+        End Try
+        Return result
     End Function
 
-    Public Function GetConsumedCalories() As Integer Implements ICalories.GetConsumedCalories
-        Return consumedCalories
-    End Function
-
-    Public Function GetCalorieLimit() As Integer Implements ICalories.GetCalorieLimit
-        Return calorieLimit
-    End Function
-    Private Function GetCurrentDateComponents() As String()
+    Private Function GetCurrentDateComponents() As Integer()
         Dim currentDate As Date = Date.Now
-        Dim dayString As String = currentDate.Day.ToString()
-        Dim monthString As String = currentDate.Month.ToString()
-        Dim yearString As String = currentDate.Year.ToString()
-
-        ' Ensure day and month are formatted as two digits if necessary (e.g., '05' instead of '5')
-        If dayString.Length = 1 Then
-            dayString = "0" & dayString
-        End If
-        If monthString.Length = 1 Then
-            monthString = "0" & monthString
-        End If
-
+        Dim dayString As Integer = currentDate.Day
+        Dim monthString As Integer = currentDate.Month
+        Dim yearString As Integer = currentDate.Year
         Return {dayString, monthString, yearString}
     End Function
 
-    Private connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Throthar\source\repos\PrjKaloriPaevik\FoodDatabase.accdb;"
-
-    Private Sub WriteData(value1 As String, value2 As String)
-        Dim dateArrey As String() = GetCurrentDateComponents()
+    Private Sub WriteData()
+        Dim dateArrey As Integer() = GetCurrentDateComponents()
         Try
             Using connection As New OleDbConnection(connectionString)
                 connection.Open()
--- PrjKaloriPaevikKalorid/ICalories.vb
+++ PrjKaloriPaevikKalorid/ICalories.vb
@@ -1,7 +1,4 @@
 ﻿Public Interface ICalories
-    Sub AddCalories(ByRef calories As Integer)
+    Sub AddCalories(ByRef foodID As Integer)
     Sub SetCalorieLimit(ByRef limit As Integer)
-    Function GetRemainingCalories() As Integer
-    Function GetConsumedCalories() As Integer
-    Function GetCalorieLimit() As Integer
 End Interface
