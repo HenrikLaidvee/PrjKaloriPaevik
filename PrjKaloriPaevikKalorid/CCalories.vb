5d099b883d9001ab6560730efc1360b4f130ad61
Miivanttu <70687706+Miivanttu@users.noreply.github.com>
2024-04-18 01:53:04 +0300

    In testing

--- PrjKaloriPaevikKalorid/CCalories.vb
+++ PrjKaloriPaevikKalorid/CCalories.vb
@@ -6,37 +6,60 @@ Public Class CCalories
 
     Private calorieLimit As Integer
     Private consumedCalories As Integer
+    Dim oldDate As Date
     Private connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Throthar\source\repos\PrjKaloriPaevik\FoodDatabase.accdb;"
-
+    Sub New()
+        Using connection As New OleDbConnection(connectionString)
+            Try
+                connection.Open()
+                Dim commandText As String = "SELECT top 1 Limiit, Kalorid, DateSerial(Päev, Kuu, Aasta) AS combined_date FROM ajalugu ORDER BY ID DESC"
+                Dim command As New OleDbCommand(commandText, connection)
+                Dim reader As OleDbDataReader = command.ExecuteReader()
+                If reader.Read() Then
+                    ' Access individual columns by column name or index
+                    calorieLimit = Convert.ToInt32(reader("Limiit"))
+                    consumedCalories = Convert.ToInt32(reader("Kalorid"))
+                    oldDate = Convert.ToDateTime(reader("combined_date"))
+                    If DateTime.Now.Date > oldDate Then
+                        consumedCalories = 0
+                    End If
+                End If
+                reader.Close()
+                connection.Close()
+            Catch ex As Exception
+                Throw New Exception("Error reading data from the database: " & ex.Message)
+            End Try
+        End Using
+    End Sub
     Public Sub SetCalorieLimit(ByRef limit As Integer) Implements ICalories.SetCalorieLimit
         calorieLimit = limit
+        WriteData()
     End Sub
 
     Public Sub AddCalories(ByRef foodID As Integer) Implements ICalories.AddCalories
+        'add consumed calories
         Dim calories As Integer = getCalories(foodID)
         consumedCalories += calories
         WriteData()
     End Sub
-    Private Function getCalories(ByRef ID As Integer)
-        Dim result As Integer
-        Try
-            Using connection As New OleDbConnection(connectionString)
-                connection.Open()
 
-                Dim commandText As String = "SELECT Kalorid FROM Toit WHERE YourPrimaryKeyColumn = ID"
-                Dim command As New OleDbCommand(commandText, connection)
-                ' Specify the value of the primary key to retrieve
-                command.Parameters.AddWithValue("ID", ID)
-                result = command.ExecuteScalar()
-                connection.Close()
-            End Using
-        Catch ex As Exception
-            'MessageBox.Show("Error: " & ex.Message)
-        End Try
+    Private Function getCalories(ByRef ID As Integer)
+        'gets calories from food
+        Dim result As Integer
+        Using connection As New OleDbConnection(connectionString)
+            connection.Open()
+            Dim commandText As String = "SELECT Kalorid FROM Toit WHERE YourPrimaryKeyColumn = ID"
+            Dim command As New OleDbCommand(commandText, connection)
+            ' Specify the value of the primary key to retrieve
+            command.Parameters.AddWithValue("ID", ID)
+            result = command.ExecuteScalar()
+            connection.Close()
+        End Using
         Return result
     End Function
 
     Private Function GetCurrentDateComponents() As Integer()
+        'converts data into day, month, year in arrey
         Dim currentDate As Date = Date.Now
         Dim dayString As Integer = currentDate.Day
         Dim monthString As Integer = currentDate.Month
