2e16c004879459f479c0dbd46a702d36eb895c9c
janml <janml@LAPTOP-JACRP4N3>
2024-04-24 18:36:52 +0300

    vahetasin console writeline message boxiks

--- FoodSafetyWarning/CFoodWarning.vb
+++ FoodSafetyWarning/CFoodWarning.vb
@@ -1,4 +1,6 @@
-﻿Public Class CFoodWarning
+﻿Imports System.Windows.Forms
+
+Public Class CFoodWarning
     Implements IFoodWarning
 
     Const fatPerCalorie As Double = 0.35
@@ -20,7 +22,7 @@
     End Function
     Public Sub DisplayWarning(foodName As String, calorieContent As Integer, fatContent As Double) Implements IFoodWarning.DisplayWarning
         If IsFoodUnhealthy(foodName, calorieContent, fatContent) > 0 Then
-            Console.WriteLine($"Warning: {foodName} is high in calories ({calorieContent} calories per serving) or contains too much fat and may not be suitable for consumption.")
+            MessageBox.Show($"Warning: {foodName} is high in calories ({calorieContent} calories per serving) or contains too much fat and may not be suitable for consumption.")
         End If
     End Sub
 
